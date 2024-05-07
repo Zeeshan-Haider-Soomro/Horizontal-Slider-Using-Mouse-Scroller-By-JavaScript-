834bf722a8dffae620d6926a5bf28cc47697acc9
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-08 00:28:22 +0500

    image slider

---
+++ app.js
@@ -0,0 +1,25 @@
+let scrollConatiner = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollConatiner.addEventListener("wheel", (evt) =>{
+    evt.preventDefault();
+    scrollConatiner.scrollLeft += evt.deltaY;
+    scrollConatiner.style.behavior = "auto";
+
+});
+
+nextBtn.addEventListener("click", ()=>{
+    scrollConatiner.style.behavior = "smoot";
+    scrollConatiner.scrollLeft += 900;
+})
+backBtn.addEventListener("click", ()=>{
+    scrollConatiner.style.behavior = "smoot";
+    scrollConatiner.scrollLeft -= 900;
+})
+
+
+
+
+
+
